208486f71be528412570ee05329d9beac12ccaba
Adam Niedzwiedz <niedzwiedz.adam@gmail.com>
2018-08-11 09:22:47 +0200

    Fix

---
+++ src/mgos_pms7003_mongoose_os_lib2_c.c
@@ -0,0 +1,5 @@
+#include <stdbool.h>
+
+bool mgos_pms7003_mongoose_os_lib2_init(void) {
+    return true;
+}
--- src/mgos_pms7003_mongoose_os_lib_c.c
+++
@@ -1,5 +0,0 @@
-#include <stdbool.h>
-
-bool mgos_pms7003_mongoose_os_lib_init(void) {
-    return true;
-}
